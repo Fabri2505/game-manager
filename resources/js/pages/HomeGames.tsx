eeedb08a6283b61929e88a7d47c5f5a9ddfbccc9
Fabricio Cervantes <fabriciocervantesmendoza@gmail.com>
2025-07-20 19:44:55 -0500

    Agregado de Header para vista home

--- resources/js/pages/HomeGames.tsx
+++ resources/js/pages/HomeGames.tsx
@@ -2,16 +2,26 @@ import { CardGame } from "@/components/home/card-game";
 
 export default function HomeGames() {
   return (
-    <div className="p-2">
-      <h1 className="text-4xl font-bold">Home Games</h1>
+    <div className=" m-5">
+      <header className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4 mb-8">
+        <div>
+          <h1 className="text-4xl font-bold tracking-tight">Home Games</h1>
+          <p className="text-muted-foreground mt-1">Juega uno de nuestros juegos</p>
+        </div>      
+      </header>
+      
 
-      <div className="flex gap-4" >
+      <div className="flex flex-wrap gap-4" >
         <CardGame 
           titulo="Golpeado" 
           descrip="Juega con una mano de 7 cartas. Veremos quien sera el donador" 
           estado="popular"
           endpoint="golpeado"/>
-        <CardGame titulo="Bingo" descrip="Juego de bingo para mas de 7 personas" estado="nuevo" endpoint="golpeado"/>
+        <CardGame 
+          titulo="Bingo" 
+          descrip="Juego de bingo para mas de 7 personas" 
+          estado="nuevo" 
+          endpoint="golpeado"/>
       </div>
     </div>
   );
